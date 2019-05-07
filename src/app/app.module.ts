cfa5c4b4fdd82e1c80485d2a40b34f5c80d1c6df
Andres Vega <andres.vega@symplifica.com>
2019-05-06 23:15:23 -0500

    Cambios en: - Se crea modulo de login - Se crea modulo de paginas - Se agregan modulos en el app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routing
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 
+// Modules
+import { LoginModule } from './login/login.module';
+import { PagesModule } from './pages/pages.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		LoginModule,
+		PagesModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Components
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+	declarations: [
+		HomeComponent
+	],
+	imports: [
+		CommonModule
+	]
+})
+export class PagesModule { }
